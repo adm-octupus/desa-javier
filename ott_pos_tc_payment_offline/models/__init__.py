4ff8d1a977892c61427a794409662d67a42f9d70
Javier Durán S <jds@octupustech.com>
2025-03-12 17:04:42 -0500

    libracion de modulos a repositorio corporativo

--- ott_pos_tc_payment_offline/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import ott_card_batch
-from . import pos_order
-from . import pos_payment_method
-from . import pos_payment
-from . import pos_session
--- ott_pos_tc_payment_offline/models/pos_payment_method.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class PosPaymentMethod(models.Model):
-    _inherit = 'pos.payment.method'
-
-    is_credit_card = fields.Boolean('Is Credit Card?', default=False)
-    ott_card_brands_ids = fields.Many2many(string="Card brands", comodel_name='ott.card.brand')
